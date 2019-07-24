02cc6fe0a2f57aa94c20a7ce50b5591ecd9a3509
Jacek Dziubiński <jacek.dziubinski@g1ant.com>
2019-07-24 16:06:55 +0200

    Move class to different file.

---
+++ G1ANT.Addon.Xlsx/Api/ColumnNamesGenerator.cs
@@ -0,0 +1,65 @@
+﻿/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Xlsx
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+using System.Linq;
+using System.Text;
+
+namespace G1ANT.Addon.Xlsx.Api
+{
+    public class ColumnNamesGenerator
+    {
+        private readonly char[] letters;
+        private readonly string[] columnNames;
+
+        public ColumnNamesGenerator()
+        {
+            letters = Enumerable.Range(0, 26).Select(x => (char)(x + 64)).ToArray();
+            columnNames = Enumerable
+                .Range(0, ushort.MaxValue)
+                .Select(x => CalcString(x))
+                .ToArray();
+        }
+           
+        public string[] GetColumnsBetweenInclusive(string column1, string column2)
+        {
+            var start = column1;
+            var end = column2;
+
+            if (start.CompareTo(end) > 0)
+            {
+                var b = end;
+                end = start;
+                start = b;
+            }
+
+            var result = columnNames
+                .SkipWhile(x => x != start)
+                .TakeWhile(x => x != end)
+                .ToList();
+
+            result.Add(end);
+
+            return result.ToArray();
+        }
+
+        private string CalcString(int index)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (index > 0)
+            {
+                sb.Append(letters[index % letters.Length]);
+                index /= letters.Length;
+            }
+
+            return new string(sb.ToString().Reverse().ToArray());
+        }
+    }
+}
+
--- G1ANT.Addon.Xlsx/Api/XlsxWrapper.cs
+++ G1ANT.Addon.Xlsx/Api/XlsxWrapper.cs
@@ -16,60 +16,9 @@ using System.IO.Packaging;
 using System.Linq;
 using DocumentFormat.OpenXml;
 using System.Windows.Forms;
-using System.Text;
 
 namespace G1ANT.Addon.Xlsx.Api
 {
-    public class ColumnNamesGenerator
-    {
-        private readonly char[] letters;
-        private readonly string[] columnNames;
-
-        public ColumnNamesGenerator()
-        {
-            letters = Enumerable.Range(0, 26).Select(x => (char)(x + 64)).ToArray();
-            columnNames = Enumerable
-                .Range(0, ushort.MaxValue)
-                .Select(x => CalcString(x))
-                .ToArray();
-        }
-           
-        public string[] GetColumnsBetweenInclusive(string column1, string column2)
-        {
-            var start = column1;
-            var end = column2;
-
-            if (start.CompareTo(end) > 0)
-            {
-                var b = end;
-                end = start;
-                start = b;
-            }
-
-            var result = columnNames
-                .SkipWhile(x => x != start)
-                .TakeWhile(x => x != end)
-                .ToList();
-
-            result.Add(end);
-
-            return result.ToArray();
-        }
-
-        private string CalcString(int index)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            while (index > 0)
-            {
-                sb.Append(letters[index % letters.Length]);
-                index /= letters.Length;
-            }
-
-            return new string(sb.ToString().Reverse().ToArray());
-        }
-    }
-    
     public class XlsxWrapper
     {
         /// <summary>
